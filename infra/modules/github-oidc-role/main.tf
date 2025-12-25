74a79e76370a02e36930c19e8b54358dab91b00c
emma-ola <iam_newton@outlook.com>
2025-12-24 20:00:40 -0500

    Refactor GitHub OIDC configuration to use `aws_iam_openid_connect_provider` data source instead of resource

--- infra/modules/github-oidc-role/main.tf
+++ infra/modules/github-oidc-role/main.tf
@@ -1,12 +1,7 @@
 data "aws_caller_identity" "current" {}
 
-resource "aws_iam_openid_connect_provider" "github" {
+data "aws_iam_openid_connect_provider" "github" {
   url = "https://token.actions.githubusercontent.com"
-
-  client_id_list = ["sts.amazonaws.com"]
-
-  # GitHub Actions OIDC thumbprint (current widely used value)
-  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
 data "aws_iam_policy_document" "assume_role" {
@@ -17,7 +12,7 @@ data "aws_iam_policy_document" "assume_role" {
 
     principals {
       type        = "Federated"
-      identifiers = [aws_iam_openid_connect_provider.github.arn]
+      identifiers = [data.aws_iam_openid_connect_provider.github.arn]
     }
 
     condition {
